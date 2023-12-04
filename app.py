643e5901365c73bb1a31cbcd528d693c95301182
Samirit <73527396+Samirit@users.noreply.github.com>
2023-12-04 13:46:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,20 +9,15 @@ from streamlit_extras.switch_page_button import switch_page
 
 from FileReader.pdfFile import PDFDBStore
 
-
+# Function to save PDF image
 @st.cache_resource
 def save_pdf_image(uploaded_file):
     pdf_bytes = uploaded_file.getvalue()
     st.session_state.pdf_bytes = pdf_bytes
-    doc = fitz.open(stream=pdf_bytes, filetype="pdf")
-    images = []
-    for i, page in enumerate(doc):  # iterate through the pages
-        zoom_x = 2  # horizontal zoom
-        zoom_y = 2  # vertical zoom
-        mat = fitz.Matrix(zoom_x, zoom_y)  # zoom factor 2 in each dimension
-        pix = page.get_pixmap(matrix=mat)  # use 'mat' instead of the identity matrix
-        img = Image.frombytes("RGB", [pix.width, pix.height], pix.samples)
-        images.append(img)
+
+    # Use context manager to close the PDF document after use
+    with fitz.open(stream=pdf_bytes, filetype="pdf") as doc:
+        images = [Image.frombytes("RGB", [pix.width, pix.height], pix.samples) for pix in doc[0]]
 
     # Combine images vertically
     long_image = Image.new("RGB", (images[0].width, sum(img.height for img in images)))
@@ -37,12 +32,12 @@ def save_pdf_image(uploaded_file):
 
     return image_bytes
 
-
+# Function to save vector store
 @st.cache_resource
 def save_vector_store(_db):
     return _db.get_vectorDB(return_docs=True)
 
-
+# Function to initialize session state
 def initialize_session_state():
     if "openai_api_key" not in st.session_state:
         st.session_state.openai_api_key = None
@@ -59,23 +54,24 @@ def initialize_session_state():
     if "pdf_bytes" not in st.session_state:
         st.session_state.pdf_bytes = None
 
+# Function to set OpenAI API key
 def set_openai_api_key(api_key):
     st.session_state["openai_api_key"] = api_key
 
-
+# Sidebar
 def sidebar():
     with st.sidebar:
         st.markdown(
             "## How to use\n"
-            "1. Enter your [OpenAI API key](https://platform.openai.com/account/api-keys) below🔑\n"  # noqa: E501
+            "1. Enter your [OpenAI API key](https://platform.openai.com/account/api-keys) below🔑\n"
             "2. Upload your PhD research paper PDF here📄\n"
         )
         api_key_input = st.text_input(
             "OpenAI API Key",
             type="password",
             placeholder="Paste your OpenAI API key here (sk-...)",
-            help="You can get your API key from https://platform.openai.com/account/api-keys.",  # noqa: E501
-            value=st.session_state.get("OPENAI_API_KEY", ""),
+            help="You can get your API key from https://platform.openai.com/account/api-keys.",
+            value=st.session_state.get("openai_api_key", ""),  # Fixed key name
         )
 
         if api_key_input:
@@ -95,38 +91,46 @@ def sidebar():
         st.markdown("Made by Amrita students()")
         st.markdown("---")
 
+# Set Streamlit page config
+st.set_page_config(
+    page_title="Research Paper Reading Assist Tool",
+    layout="centered",
+    initial_sidebar_state="expanded",
+)
 
-st.set_page_config(page_title="Research Paper Reading Assist Tool",
-                   layout="centered",
-                   initial_sidebar_state="expanded")
+# Remove Page Navigation
+st.markdown("<style> ul {display: none;} </style>", unsafe_allow_html=True)
 
-st.markdown("<style> ul {display: none;} </style>", unsafe_allow_html=True)  # Removes Page Navigation
-
+# Main title
 st.title("Virtual PhD Research Assistant 📄")
-sidebar()
+
+# Initialize session state
 initialize_session_state()
 
 # Page 1 - Upload PDF
 st.header("Upload your Research paper PDF document")
 uploaded_file = st.file_uploader("Choose a PDF file", type="pdf")
 
+# Process uploaded file
 if uploaded_file is not None:
 
+    # Create PDFDBStore instance
     pdfDB = PDFDBStore(uploaded_file)
 
+    # Check OpenAI API key
     if st.session_state.openai_api_key is None:
         st.error("Please enter your OpenAI API key in the sidebar to continue.")
-
     elif not pdfDB.is_valid_key():
         st.error("Invalid OpenAI API key. Please enter a valid key in the sidebar to continue.")
-
     else:
         st.success("OpenAI API key set successfully!")
 
+        # Process PDF File
         with st.spinner("Processing PDF File...This may take a while⏳"):
             st.session_state.vector_store, st.session_state.document_chunks = save_vector_store(pdfDB)
             st.session_state.pdf_image = save_pdf_image(uploaded_file)
 
+        # Display success message and switch to 'results' page
         st.success("PDF uploaded successfully!")
         st.session_state.vector_store, st.session_state.document_chunks = save_vector_store(pdfDB)
         st.session_state.pdf_image = save_pdf_image(uploaded_file)
